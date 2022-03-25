7939edeb4992c6843d54f2aaaf7ae41f6bcfdc3f
DESKTOP-3MUR0D2\33615 <kewou.noumia@gmail.com>
2022-03-25 19:23:32 +0100

    Initialisation du projet

---
+++ src/test/java/com/natixis/noumiaTest/NoumiaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natixis.noumiaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoumiaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
